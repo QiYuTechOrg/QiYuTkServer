3ca48283029072bcec97fe08d5a9146912875681
dev <dev@qiyutech.tech>
2021-06-28 14:12:48 +0800

    fix for django ninja error

--- tbk/core/vendor/ztk/ztk_v2.py
+++ tbk/core/vendor/ztk/ztk_v2.py
@@ -11,13 +11,9 @@ from ...logger import get_logger
 
 __all__ = ["get_ztk_api_v2"]
 
-g_ztk: Optional[ZTKStd] = None
-
 
 def get_ztk_api_v2(logger: Optional[structlog.stdlib.BoundLogger] = None):
     if logger is None:
         logger = get_logger()
-    global g_ztk
-    if g_ztk is None:
-        g_ztk = ZTKStd(SConfig.ZTKSid, logger)
-    return g_ztk
+
+    return ZTKStd(SConfig.ZTKSid, logger)
