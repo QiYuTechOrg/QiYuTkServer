28fa0f9e7b6835d1bd238f0c6dfe118189ff6966
dev <dev@qiyutech.tech>
2021-07-01 21:40:38 +0800

    移除废弃的安装包

--- tbk/core/vendor/top/args/__init__.py
+++
@@ -1,5 +0,0 @@
-from .product_get import ProductGetArgs  # noqa
-from .tbk_sc_publisher_info_save import TBKScPublisherInfoSaveArgs  # noqa
-from .time_get import TimeGetArgs  # noqa
-from .top_auth_token_create import TopAuthTokenCreateArgs  # noqa
-from .user_seller_get import UserSellerGetArgs  # noqa
--- tbk/core/vendor/top/resp/__init__.py
+++
@@ -1,3 +0,0 @@
-from .tbk_sc_publisher_info_save import TBKScPublisherInfoSaveResp  # noqa
-from .time_get import TimeGetResp  # noqa
-from .top_auth_token_create import TopAuthTokenCreateResp  # noqa
