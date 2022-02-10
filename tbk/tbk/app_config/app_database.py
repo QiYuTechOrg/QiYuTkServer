b36e9c385cd349cf67395c7a2edccc8a1151925a
dev <dev@qiyutech.tech>
2022-02-10 10:37:59 +0800

    Use SQLite3 in sqlite directory

--- tbk/tbk/app_config/app_database.py
+++ tbk/tbk/app_config/app_database.py
@@ -22,6 +22,6 @@ else:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(os.path.dirname(__file__), "../../db.sqlite3"),
+            "NAME": os.path.join(os.path.dirname(__file__), "../../../sqlite/db.sqlite3"),
         }
     }
