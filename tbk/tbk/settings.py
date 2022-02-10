b75981854c52042b955591535808a2cd9002e20b
dev <dev@qiyutech.tech>
2022-02-10 11:23:20 +0800

    add locale

--- tbk/tbk/settings.py
+++ tbk/tbk/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
